28bae412dfbb8a4855e769b6b632dd68e030d913
amsprimedigitalcore-hue <amsprimedigitalcore@gmail.com>
2025-08-10 10:22:42 -0700

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -21,7 +21,7 @@ const Footer: React.FC = () => {
             <img
               src="https://i.postimg.cc/FHB1kmL5/ASSistaura-logo.jpg"
               alt="AssistAura Logo"
-              className="w-20 h-auto"
+          className="w-[120px] md:w-[150px] h-auto"
             />
             <div className="flex space-x-4">
               <a
