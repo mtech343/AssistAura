f0e280c2249b56c3ab4a1c7f9fa7df4e3373823f
amsprimedigitalcore-hue <amsprimedigitalcore@gmail.com>
2025-08-10 10:26:11 -0700

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -42,7 +42,7 @@ const Header: React.FC = () => {
             <img
               src="https://i.postimg.cc/FHB1kmL5/ASSistaura-logo.jpg"
               alt="AssistAura Logo"
-              className="w-[100px] md:w-[80px] h-auto"
+              className="w-[100px] md:w-[90px] h-auto"
             />
           </Link>
 
